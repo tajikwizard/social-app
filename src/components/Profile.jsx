2a4f56e2b68e65816b9d5d86d5602c24f59052c7
tajikwizard <abdulazizcodeshere@gmail.com>
2024-03-13 09:41:44 +0300

    css module and folder structure

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Profile = ()  =>{
+  return (
+    <main className='main'>
+        <div>
+        <img img='alt' src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1292351815.1710201600&semt=ais" alt="img" srcset="" />
+        </div>
+        <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpktEUbSrXtXSos_wksTsAJa-pmmnjlI4SBqz7YEzxt9BvtxGdLWwO7lYGrW2orerZxhk&usqp=CAU" alt='img' />
+        </div>
+        <div>
+        My POST
+        <div>
+            new Post
+        </div>
+        <div>
+            POST 1
+        </div>
+        <div>
+            POST 2
+        </div>
+        </div>
+   </main>
+  )
+}
+
+export default Profile
